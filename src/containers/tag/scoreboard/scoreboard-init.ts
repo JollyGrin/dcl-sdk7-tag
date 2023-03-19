4c71ead25f547e2f32104f864b6e9081c69c440d
JollyGrin <jollygrin@protonmail.com>
2023-03-19 02:07:55 +0100

    scoreboard

--- src/containers/tag/scoreboard/scoreboard-init.ts
+++ src/containers/tag/scoreboard/scoreboard-init.ts
@@ -1,29 +1,47 @@
-import { engine, Entity, MeshRenderer, TextShape, Transform } from "@dcl/sdk/ecs"
-import { Quaternion, Vector3 } from "@dcl/sdk/math"
+import { engine, Entity, MeshRenderer, TextShape, Transform } from '@dcl/sdk/ecs'
+import { Quaternion, Vector3 } from '@dcl/sdk/math'
+import { PickupSphere } from '../pickup/pickup-components'
+import { ScorboardContainer } from './scoreboard-components'
 
 export const CreateScoreboard = () => {
-    const wall = ScoreboardWall()
-    const redText = TeamText(wall, 'red')
+  const wall = ScoreboardWall()
+  const redText = TeamText(wall, 'red')
+
+  engine.addSystem(RedLength)
 }
 
 const ScoreboardWall = (): Entity => {
-    const scoreboard = engine.addEntity()
-    MeshRenderer.setPlane(scoreboard)
-    Transform.create(scoreboard, {
-        position: Vector3.create(18, 2, 18),
-        rotation: Quaternion.fromEulerDegrees(0, 90, 0),
-        scale: Vector3.create(6, 3, 1)
-    })
-    return scoreboard
+  const scoreboard = engine.addEntity()
+  MeshRenderer.setPlane(scoreboard)
+  Transform.create(scoreboard, {
+    position: Vector3.create(0.5, 2, 18),
+    rotation: Quaternion.fromEulerDegrees(0, 90, 0),
+    scale: Vector3.create(6, 3, 1)
+  })
+  return scoreboard
 }
 
 const TeamText = (parent: Entity, team: 'red' | 'blue') => {
-    const text = engine.addEntity()
-    TextShape.create(text, {
-        text: team
-    })
-    Transform.create(text, {
-        parent: parent,
-        scale: Vector3.create(1, 1, 1)
-    } )
+  const Spheres = engine.getEntitiesWith(PickupSphere)
+  const text = engine.addEntity()
+  ScorboardContainer.text.create(text)
+  TextShape.create(text, {
+    text: team + ' - ' + [...Spheres].length,
+    fontSize: 2
+  })
+  Transform.create(text, {
+    parent: parent,
+    position: Vector3.create(0, 0, 0.05),
+    scale: Vector3.create(0.4, 1, 1)
+  })
+}
+
+const RedLength = () => {
+  const Spheres = engine.getEntitiesWith(PickupSphere)
+  const SpheresLength = [...Spheres].length
+  console.log('qqqqq', SpheresLength)
+  const Text = engine.getEntitiesWith(ScorboardContainer.text)
+  for (const [entity] of Text) {
+    TextShape.getMutable(entity).text = `Red: ${SpheresLength}`
+  }
 }
